47b54c6c45aa626a045cf7ef0efd15c5c95b4f3b
Gary2Luke <240158040@qq.com>
2017-09-04 11:15:55 +0800

    Create 字符串排列组合.cpp

---
+++ 字符串排列组合.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+/*字符串排列*/
+void Permutation(string &str, int start){
+	if(start == str.size() - 1){
+		cout << str << " ";
+		return;
+	}
+	for(int i = start; i < str.size(); i++){
+		if(i != start && str[i] == str[start])
+			continue;
+		swap(str[i], str[start]);	
+		Permutation(str, start+1);
+		swap(str[i], str[start]);	
+	}
+}
+
+/*字符串组合*/
+void Help(string &str, vector<char> &tmp, int len, int index){
+	if(len == 0){
+		for(int i = 0; i < tmp.size(); i++)
+			cout << tmp[i];
+		cout << " ";
+		return;
+	}
+	if(index == str.size())
+		return;
+	tmp.push_back(str[index]);
+	Help(str, tmp, len-1, index+1);
+	tmp.pop_back();
+	Help(str, tmp, len, index+1);
+
+}
+
+void Combination(string &str){
+	vector<char> tmp;
+	for(int i = 1; i <= str.size(); i++)
+		Help(str, tmp, i, 0);
+}
+int main(){
+	string str = "abc";
+	//Permutation(str, 0);
+	Combination(str);
+	system("pause");
+	return 0;
+}
