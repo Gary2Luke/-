cc28dc4e45fcefc02891f3656ebdee1bcba2f97c
Gary2Luke <240158040@qq.com>
2017-10-18 10:45:37 +0800

    Update 字符串排列组合.cpp

--- 字符串排列组合.cpp
+++ 字符串排列组合.cpp
@@ -1,49 +1,54 @@
-#include <iostream>
-#include <string>
-#include <vector>
+#include<iostream>
+#include<vector>
+#include<string>
 
 using namespace std;
-/*字符串排列*/
-void Permutation(string &str, int start){
-	if(start == str.size() - 1){
+
+void Permutation(string &str, int start) {
+	if (start == str.size() - 1) {
 		cout << str << " ";
 		return;
 	}
-	for(int i = start; i < str.size(); i++){
-		if(i != start && str[i] == str[start])
+	for (int i = start; i < str.size(); i++) {
+		if (i != start && str[i] == str[start]) {
 			continue;
-		swap(str[i], str[start]);	
-		Permutation(str, start+1);
-		swap(str[i], str[start]);	
+		}
+		else {
+			swap(str[i], str[start]);
+			Permutation(str, start + 1);
+			swap(str[i], str[start]);
+		}
 	}
 }
 
-/*字符串组合，字符串组合只适用于字符串各个元素不相等时的情况*/
-void Help(string &str, vector<char> &tmp, int len, int index){
-	if(len == 0){
-		for(int i = 0; i < tmp.size(); i++)
+void Combine(string& str, vector<char>& tmp, int len, int start) {
+	if (len == 0) {
+		for (int i = 0; i < tmp.size(); i++)
 			cout << tmp[i];
 		cout << " ";
 		return;
 	}
-	if(index == str.size())
-		return;
-	tmp.push_back(str[index]);
-	Help(str, tmp, len-1, index+1);
-	tmp.pop_back();
-	Help(str, tmp, len, index+1);
-
+	for (int i = start; i < str.size(); i++) {
+		tmp.push_back(str[i]);
+		Combine(str, tmp, len - 1, i + 1);
+		tmp.pop_back();
+	}
 }
 
-void Combination(string &str){
+void Combination(string &str) {
 	vector<char> tmp;
-	for(int i = 1; i <= str.size(); i++)
-		Help(str, tmp, i, 0);
+	for (int i = 1; i <= str.size(); i++)
+		Combine(str, tmp, i, 0);
 }
-int main(){
-	string str = "abc";
-	//Permutation(str, 0);
+
+int main() {
+	string str = "abcd";
+	cout << "字符串" << str << "的排列为 : " << endl;
+	Permutation(str, 0);
+	cout << endl;
+	cout << "字符串" << str << "的组合为 : " << endl;
 	Combination(str);
+	cout << endl;
 	system("pause");
 	return 0;
 }
